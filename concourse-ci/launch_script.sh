64c5d48d2163e7ec4667bf351c238bf0caf6ddea
Tom Anderson <toanderson@pivotal.io>
2016-04-26 18:28:01 +0100

    Again attempt to strengthen Concourse launch script logging

--- concourse-ci/launch_script.sh
+++ concourse-ci/launch_script.sh
@@ -2,16 +2,21 @@
 
 echo "Launch script running as $(whoami) at $(date) in $(pwd)"
 
+echo "Downloading scripts ..."
 for f in concourse.yml bosh.yml create_cert.sh setup_devbox.py init.sh deploy_bosh.sh
 do
    wget $1/$f -O $f
 done
 
+echo "Copying scripts ..."
 cp * ../../
+
+echo "Relocating ..."
 cd ../../
 
 echo "---- $(pwd):"
 ls -lF
 echo "----"
 
+echo "Running setup ..."
 python setup_devbox.py
