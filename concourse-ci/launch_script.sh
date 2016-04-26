b09d421ec4ec8503b9dca896ba5faa52abf0144b
Tom Anderson <toanderson@pivotal.io>
2016-04-26 17:22:51 +0100

    Add some diagnostic logging to the Concourse launch script

--- concourse-ci/launch_script.sh
+++ concourse-ci/launch_script.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -eu
 
+echo "Launch script running as $(whoami) at $(date) in $(pwd)"
+
 for f in concourse.yml bosh.yml create_cert.sh setup_devbox.py init.sh deploy_bosh.sh
 do
    wget $1/$f -O $f
@@ -7,5 +9,9 @@ done
 
 cp * ../../
 cd ../../
+
+echo "---- $(pwd):"
+ls -lF
+echo "----"
 
 python setup_devbox.py
